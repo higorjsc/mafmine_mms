988be19a5dfe31b7876e37efd99340f108c3d921
HIGOR J. S. CAMPOS <hh1217higorcampos@hotmail.com>
2023-07-10 20:33:53 -0300

    .

--- rmr_calculadora/rmr_design.js
+++ rmr_calculadora/rmr_design.js
@@ -12,7 +12,7 @@ function Display_none(id) {
 //EVENTO de troca do select visível da rock strenght
 function Display_strenght() {
     const strenght = document.getElementById("select-strenght").value
-    
+
     Display_none("point-load")
     Display_none("ucs")
     Display_block(strenght)
@@ -53,6 +53,10 @@ function Inserir_titulo() {
     titulo.innerText += texto[(url[2] + idioma)]
 }
 
+function Balao_sai() {
+    const balao = document.getElementById("balao")
+    balao.style.display = "none"
+}
 
 function Positions(entry, messages, positions) {
 
@@ -62,67 +66,45 @@ function Positions(entry, messages, positions) {
         balao.innerText = messages[entry]
         balao.style.display = "block"
     }
-
     //Posiciona o balão conforme o dicionário de posições
     if (positions[entry]) {
         const { x, y } = positions[entry]
         balao.style.transform = `translate(${x}px, ${y}px)`
     } else {
-        balao.style.transform = "translate(-100px,-75px)"
+        balao.style.transform = "translate(-200px,-80px)"
     }
 
 }
 
-
-function Balao_entra(entry, idioma) {
-
-    let messages 
-    if (idioma == "en") {
+function Balao_entra(entry) {
+    const idioma = Obter_idioma()
+    let messages
+    if (idioma == "pt") {
         messages = {
-            "matriz": "Pair-wise comparision matrix",
-            "pesos": "AHP calculated weights",
-            "cons": "Consistence vector",
-            "lambda": "Arithmetic mean of the consistent vector and maximum eigenvalue of the matrix",
-            "ci": "Consistence index of pair-wise comparision matrix",
-            "ri": "Consistency index of a randomly generated reciprocal matrix. (Saaty, 1991)",
-            "cr": "CR < 0.1 → Consistent matrix\nCR > 0.1 → Inconsistent matrix",
-            "botao-help": "Check method",
-            "slider-geo-ob": "Importance of geometry\nrelative to ore body",
-            "slider-geo-hw": "Importance of geometry\nrelative to hanging wall",
-            "slider-geo-fw": "Importance of geometry\nrelative to footwall",
-            "slider-ob-hw": "Importance of ore body\nrelative to hanging wall",
-            "slider-ob-fw": "Importance of ore body\nrelative to footwall",
-            "slider-hw-fw": "Importance of hanging wall\nrelative to footwall"
+            "select-strenght": "Calcular por UCS ou Carga Pontual?",
+            "ucs": "Resistência a compressão uniaxial/simples.",
+            "point-load": "Resistência a compressão por cargas concentradas.",
+            "spacing": "Espaçamento entre as juntas/fraturas.",
+            "comprimento": "Comprimento das descontinuidades.",
+            "separacao": "Espessura da abertura (ranhura) da fratura.",
+            "preenchimento": "Duro: quartzo, piritas, rochas ígneas, etc.\n\nMacio: argilas, siltes, materiais decompostos, etc.\n\n",
         }
     } else {
         messages = {
-            "matriz": "Matriz de julgamento",
-            "pesos": "Pesos calculados pelo método",
-            "cons": "Vetor Consistencia",
-            "lambda": "Média aritimética do vetor consistencia e\nmaior autor valor da matriz de julgamento",
-            "ci": "Indice de consistencia da matriz de julgamento ",
-            "ri": "índice de consistência de uma matriz recíproca\ngerada randomicamente. (Saaty, 1991)",
-            "cr": "CR < 0.1 → Matriz de julgamento consistente\nCR > 0.1 → Matriz de julgamento inconsistente",
-            "botao-help": "Verificar Método",
-            "slider-geo-ob": "Importancia da geometria em\nrelação ao corpo de minério",
-            "slider-geo-hw": "Importancia da geometria em\nrelação ao hanging wall",
-            "slider-geo-fw": "Importancia da geometria em\nrelação ao footwall",
-            "slider-ob-hw": "Importancia do corpo de minerio\nem relação ao hanging wall",
-            "slider-ob-fw": "Importancia do corpo de minerio\nem relação ao footwall",
-            "slider-hw-fw": "Importancia do hanging wall\nem relação ao footwall",
+            "select-strenght": "Calcular por UCS ou Carga Pontual?",
+            "ucs": "Resistência a compressão uniaxial/simples.",
+            "point-load": "Resistência a compressão por cargas concentradas.",
+            "spacing": "Espaçamento entre as juntas/fraturas.",
+            "comprimento": "Comprimento das descontinuidades.",
+            "separacao": "Espessura da abertura (ranhura) da fratura.",
+            "preenchimento": "Duro: quartzo, piritas, rochas ígneas, etc.\n\nMacio: argilas, siltes, materiais decompostos, etc.\n\n",
         }
     }
-
     const positions = {
-        "matriz": { x: -100, y: -50 },
-        "pesos": { x: -100, y: -50 },
-        "cons": { x: -100, y: -50 },
+        "select-strenght": { x: -200, y: -75 },
+        "point-load": { x: -200, y: -75 },
+        "preenchimento": { x: -250, y: -75 },
     }
     //Mostra e posiciona o balão conforme o dicionário de posições
     Positions(entry, messages, positions)
-}
-
-function Balao_sai() {
-    const balao = document.getElementById("balao")
-    balao.style.display = "none"
 }
