36f4fcb6cc59e6f405a1540cfb50ea1e666d12fd
HIGOR J. S. CAMPOS <hh1217higorcampos@hotmail.com>
2023-09-02 11:26:49 -0300

    Update design.js

--- src/assets/javaScript/design.js
+++ src/assets/javaScript/design.js
@@ -63,7 +63,7 @@ function Mudar_imagem() {
     const nome_imagem = forma_geral + "_" + distribuicao + ".png"
 
     //Adiciona o diretório ao elemento <img> da seção 2
-    ilustracao.src = "\\src\\assets\\Imagens\\" + nome_imagem
+    ilustracao.src = "assets/Imagens/" + nome_imagem
         
     const superficie = document.getElementById("superficie")
     superficie.style.display = "block"
